71959073fc5fc875ddf55afb1b75b88189b04ce4
elnarmen <101528347+elnarmen@users.noreply.github.com>
2022-12-14 22:14:31 +0300

    add 'verbose_name' to each field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=50)
-    title_jp = models.CharField(max_length=50)
-    description = models.TextField(max_length=1000, blank=True)
-    image = models.ImageField(null=True)
+    title = models.CharField(max_length=200, verbose_name='Имя')
+    title_en = models.CharField(max_length=50, verbose_name='Английское имя')
+    title_jp = models.CharField(max_length=50, verbose_name='Японское имя')
+    description = models.TextField(max_length=1000, blank=True, verbose_name='Описание')
+    image = models.ImageField(null=True, verbose_name='Изображение')
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
@@ -21,13 +21,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(default=1)
-    health = models.IntegerField(default=100)
-    strength = models.IntegerField(default=100)
-    defence = models.IntegerField(default=100)
-    stamina = models.IntegerField(default=100)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(verbose_name='Дата и время появления')
+    disappeared_at = models.DateTimeField(verbose_name='Дата и время изчезновения')
+    level = models.IntegerField(default=1, verbose_name='Уровень')
+    health = models.IntegerField(default=100, verbose_name='Здоровье')
+    strength = models.IntegerField(default=100, verbose_name='Атака')
+    defence = models.IntegerField(default=100, verbose_name='Защита')
+    stamina = models.IntegerField(default=100, verbose_name='Выносливость')
+
+    def __str__(self):
+        return f'{self.pokemon}-{self.pk}'
